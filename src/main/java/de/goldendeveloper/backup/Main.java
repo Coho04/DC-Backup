98eecfb800ae4b7acb3250006f61995104a54b66
_Coho04_ <Cohohohn04@gmail.com>
2024-01-04 23:53:50 +0100

    Update Main.java
    Signed-off-by: _Coho04_ <Cohohohn04@gmail.com>

--- src/main/java/de/goldendeveloper/backup/Main.java
+++ src/main/java/de/goldendeveloper/backup/Main.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Main {
 
     public static void main(String[] args) {
-        DCBotBuilder builder = new DCBotBuilder(args, false);
+        DCBotBuilder builder = new DCBotBuilder(args, true);
         builder.registerGatewayIntents(GatewayIntent.MESSAGE_CONTENT);
         builder.registerCommands(new BackupCommand(), new ImportCommand());
         builder.build();
